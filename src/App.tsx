a94b49b975acec58c3d03131055b19617f9abb75
Henry Orjuela <henryalejo@gmail.com>
2024-08-16 12:25:36 -0600

    init: to do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,34 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-interface Todo {
-  id: string;
-  title: string;
-  done: boolean;
-}
-
-interface RenderTodo extends Todo {
-  handlerDelete: (id: string) => void;
-}
-
-const Item = (props: RenderTodo) => {  
-  const {id, title, done, handlerDelete} = props;
-  return (
-    <div className="card">
-      <div>{title}</div>
-      <button onClick={()=>handlerDelete(id)}>Delete</button> 
-    </div>)
-};
-
-const List = () => {  
-  const [items, setItems] = useState<Array<Todo>>([]);
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const handlerAdd = () => {
-    if (inputRef.current) {
-      const title = inputRef.current.value;
-      const newItems = [...items, {id: Date.now().toString(), title, done: false}];
-      setItems(newItems);
-      inputRef.current.value = '';
-    }
-  };
-
-  const handlerDelete = (id: string) => {
-    const newItems = items.filter((item) => item.id !== id);
-    setItems(newItems);
-  }
-
-  return  (
-    <div className='list'>
-      <h1>TO DO APP</h1>
-      <div className="inputContainer"><input ref={inputRef} type="text" placeholder="Add a task" />
-      <button onClick={() =>handlerAdd()}>Add</button> </div>
-      {items.map((item) => (
-        <Item key={item.id} {...item} handlerDelete={handlerDelete}/>
-      ))}
-    </div>
-  );
-}
-
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <List/>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
